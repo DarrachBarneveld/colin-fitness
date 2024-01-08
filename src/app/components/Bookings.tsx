61e999a4d84ca35d55cce239cc71578ca790ef41
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-01-08 16:36:57 +0000

    Added bookings section text

--- src/app/components/Bookings.tsx
+++ src/app/components/Bookings.tsx
@@ -11,15 +11,29 @@ interface BookingsProps {}
 const Bookings: FunctionComponent<BookingsProps> = () => {
   const [startDate, setStartDate] = useState(new Date());
 
-  console.log(startDate);
-
   return (
-    <DatePicker
-      selected={startDate}
-      onChange={(date) => setStartDate(date as Date)}
-      excludeDates={[addDays(new Date(), 1), addDays(new Date(), 5)]}
-      inline
-    />
+    <section className="container p-8 mx-auto">
+      {/* Testimonials */}
+      <div className="text-center p-4">
+        <h2 className="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-semibold text-slate-50">
+          Bookings
+        </h2>
+
+        <p className="text-slate-400 max-w-xl mx-auto">
+          Push your fitness further with our mix of facilities and we&apos;ll
+          support you with advice on new and better ways to train.
+        </p>
+      </div>
+      <div className="flex justify-center items-center p-4">
+        <DatePicker
+          className="mx-auto"
+          selected={startDate}
+          onChange={(date) => setStartDate(date as Date)}
+          excludeDates={[addDays(new Date(), 1), addDays(new Date(), 5)]}
+          inline
+        />
+      </div>
+    </section>
   );
 };
 export default Bookings;
